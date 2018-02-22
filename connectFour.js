a66e315a21b1dd32bb494876a9b9d93653058fd6
Bob Hunt <bob.hunt.email@gmail.com>
2018-02-22 17:08:03 -0500

    added 7 columns & an event listener.

--- connectFour.js
+++ connectFour.js
@@ -1,23 +1,63 @@
 
-var x=document.getElementById("background");
+let x=document.getElementById("background");
+
+
 for(i=0;i<7;i++){
     let rectangle=document.createElement("div");
     rectangle.setAttribute("class","column");
-    var columnId="column" + i;
+    let columnId="column" + i;
     rectangle.setAttribute("id", columnId);
 
     x.appendChild(rectangle);
 
+    rectangle.addEventListener("click", clickEvent);
+    function clickEvent(event){
+        let clickLocation=event.target;
+        console.log(clickLocation);
+    }
+    
+
 }
 
-// var y=document.getElementById("column");
-// for(j=0;j<6;j++){
-//     let box=document.createElement("div");
-//     box.setAttribute("class","square");
-//     var squareId="square" + j;
-//     box.setAttribute("id", squareId);
 
-//     y.appendChild(squareId);
+
+
+    
+// handleClick = function (event) {
+//     var cell = event.target;
+
+//     if (cell.innerHTML.length > 0) { 
+//         alert("Select a different square");
+//     }
+//     cell.innerHTML = currentPlayer;
+//     moveCount = moveCount + 1;
+
+//     if (currentPlayer === "X") {
+//         playerSelections = playerXSelections;
+//         nextPlayer = "O";
+//     } else {
+//         playerSelections = playerOSelections;
+//         nextPlayer = "X";
+//     }
+
+//     playerSelections.push(Number(cell.id));
+
+//     const alertWin = function() {
+//         alert(winMessage);
+//         console.log(playerSelections);
+//     }
+
+//     if (checkWin(playerSelections)) {
+//         window.setTimeout(alertWin, 300);
+//     } else {
+//         if(moveCount == 9){
+//             alert(drawMessage);
+//         }  
+//     }
+
+
+//     // Swap players
+
+//     currentPlayer = nextPlayer;
 
 // }
-    
