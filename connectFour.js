21583f52dd11a7d5db8a8382948bf48a0821d7fc
Bob Hunt <bob.hunt.email@gmail.com>
2018-02-25 16:31:45 -0500

    still working on determining a winner

--- connectFour.js
+++ connectFour.js
@@ -15,9 +15,14 @@ for (let i=0;i<7;i++){
 
  // create an action whenever user clicks & identify the clicked column //
     rectangle.addEventListener("click", clickEvent);
-    
-
 }
+// console.log(background);
+// console.log(columnPlacement);
+let column0 = document.getElementById("column-0");
+    // console.log(column0);
+
+
+
 
 function clickEvent(event) {
     const column = event.currentTarget;
@@ -27,9 +32,9 @@ function clickEvent(event) {
     const squareCountInColumn = squaresInColumn.length;
 
 
-    console.log(column);
-    console.log(columnIndex);
-    console.log(squareCountInColumn);
+    console.log(column0);
+    // console.log(columnIndex);
+    // console.log(squareCountInColumn);
 
 // ...
 
@@ -39,22 +44,79 @@ function clickEvent(event) {
         box.classList.add("square")
         box.classList.add(currentPlayerPiece);
         let j=(squareCountInColumn);
-        console.log(j);  
+        // console.log(j);  
         let squareId="square" + j;
     // unsuccessful attempt to limit the number of divs to 6 per column. //
     // received an "illegal break" message. //
         // if (squareId>6){
         //     break;
         // }
-        console.log(squareId);
+
+        // console.log(squareId);
         box.id=squareId;
         column.appendChild(box);
 
+    // Determine if a winner exists //
+    // Step 1: Singling out and searching each column //
+        
+        for (let k=0; k<7; k++){
+            let searchedColumn = "column-" + k;
+            let childCount = searchedColumn.document;
+            // console.log(childCount);
+            // console.log(column);
+            console.log(column0[k]);
+            console.log(column0.length);
+        
+        }
+
 
 // alternate class attributes //
-    let playerPieceHolder=currentPlayerPiece;
+    let playerPieceTemporaryHolder=currentPlayerPiece;
     currentPlayerPiece=nextPlayerPiece;
-    nextPlayerPiece=playerPieceHolder;
+    nextPlayerPiece=playerPieceTemporaryHolder;
   
 }
 
+
+
+
+// //**********************************************************************//
+// // Sample Animation: cut & pasted from web (for future experimentation) //
+// #myContainer {
+//     width: 400px;
+//     height: 400px;
+//     position: relative;
+//     background: yellow;
+//   }
+//   #myAnimation {
+//     width: 50px;
+//     height: 50px;
+//     position: absolute;
+//     background-color: red;
+//   }
+//   </style>
+//   <body>
+  
+//   <p>
+//   <button onclick="myMove()">Click Me</button> 
+//   </p>
+  
+//   <div id ="myContainer">
+//   <div id ="myAnimation"></div>
+//   </div>
+  
+//   <script>
+//   function myMove() {
+//     var elem = document.getElementById("myAnimation");   
+//     var pos = 0;
+//     var id = setInterval(frame, 10);
+//     function frame() {
+//       if (pos == 350) {
+//         clearInterval(id);
+//       } else {
+//         pos++; 
+//         elem.style.top = pos + 'px'; 
+//         elem.style.left = pos + 'px'; 
+//       }
+//     }
+//   }
